edc31cab0bdab9198dca4c492fb4aeaab13fd775
Dario Djuric <dario@thisdot.co>
2022-11-10 00:19:20 +0100

    Simplify Next Auth config

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,25 +7,10 @@ export const authOptions = {
     CredentialsProvider({
       // The name to display on the sign in form (e.g. 'Sign in with...')
       name: 'Credentials',
-      // The credentials is used to generate a suitable form on the sign in page.
-      // You can specify whatever fields you are expecting to be submitted.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
-      credentials: {
-        username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
-        password: { label: 'Password', type: 'password' },
-      },
-      async authorize(credentials, req) {
+      async authorize() {
         return {
           username: 'dario',
         };
-      },
-      async redirect({ url, baseUrl }) {
-        // Allows relative callback URLs
-        if (url.startsWith('/')) return `${baseUrl}${url}`;
-        // Allows callback URLs on the same origin
-        else if (new URL(url).origin === baseUrl) return url;
-        return baseUrl;
       },
     }),
   ],
@@ -35,17 +20,8 @@ export const authOptions = {
     signOut: '/my-profile',
   },
   callbacks: {
-    async signIn({ user, account, profile, email, credentials }) {
-      return true;
-    },
     async redirect({ url, baseUrl }) {
       return baseUrl;
-    },
-    async session({ session, user, token }) {
-      return session;
-    },
-    async jwt({ token, user, account, profile, isNewUser }) {
-      return token;
     },
   },
 };
