92127fc7ad8f5e8b48710fe2013b4ee0a5b0f701
Dario Djuric <dario@thisdot.co>
2023-01-12 11:59:44 +0100

    Add footer text

--- components/UnauthenticatedLayout.tsx
+++ components/UnauthenticatedLayout.tsx
@@ -1,6 +1,15 @@
-import { Container, Heading, Image, Link, VStack } from '@chakra-ui/react';
+import {
+  Box,
+  Container,
+  Heading,
+  Image,
+  Link,
+  VStack,
+  Text,
+} from '@chakra-ui/react';
 import { ReactNode } from 'react';
 import NextLink from 'next/link';
+import { TextLink } from './TextLink';
 
 interface Props {
   children: ReactNode;
@@ -8,18 +17,33 @@ interface Props {
 
 const UnauthenticatedLayout = ({ children }: Props) => {
   return (
-    <Container mt="10" h="full">
-      <VStack spacing={25}>
-        <VStack w="full">
-          <NextLink legacyBehavior href="/" passHref>
-            <Link>
-              <Image boxSize="150" src="/more-reps-logo.svg" alt="More Reps" />
-            </Link>
-          </NextLink>
+    <Container h="full">
+      <VStack h="full">
+        <VStack spacing={25} flexGrow="2">
+          <VStack w="full" pt="25">
+            <NextLink legacyBehavior href="/" passHref>
+              <Link>
+                <Image
+                  boxSize="150"
+                  src="/more-reps-logo.svg"
+                  alt="More Reps"
+                />
+              </Link>
+            </NextLink>
 
-          <Heading as="h1">More Reps</Heading>
+            <Heading as="h1">More Reps</Heading>
+          </VStack>
+          {children}
         </VStack>
-        {children}
+        <Box pb="30" textAlign="center">
+          <Text fontSize="xs">
+            More Reps is an{' '}
+            <TextLink href="https://github.com/dariodjuric/more-reps">
+              open-source project
+            </TextLink>{' '}
+            by <TextLink href="https://darios.blog">Dario Djuric</TextLink>.
+          </Text>
+        </Box>
       </VStack>
     </Container>
   );
